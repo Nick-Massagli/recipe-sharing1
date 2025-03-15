16cceb065d72ee110a81ceab43c477f0cda74358
jm-connell <con18013@byui.edu>
2025-03-15 17:18:42 -0600

    update response status codes

--- controllers/users.js
+++ controllers/users.js
@@ -17,8 +17,12 @@ const getUserById = async (req, res) => {
     .collection("users")
     .find({ _id: userId });
   result.toArray().then((lists) => {
-    res.setHeader("Content-Type", "application/json");
-    res.status(200).json(lists[0]);
+    if (lists.length > 0) {
+      res.setHeader("Content-Type", "application/json");
+      res.status(200).json(lists[0]);
+    } else {
+      res.status(404).json({ error: "User not found" });
+    }
   });
 };
 
@@ -35,11 +39,11 @@ const createUser = async (req, res) => {
     .collection("users")
     .insertOne(user);
   if (response.acknowledged) {
-    res.status(201).json(response);
-  } else {
     res
-      .status(500)
-      .json(response.error || "Some error occurred while creating the user.");
+      .status(201)
+      .json({ message: "User created successfully", id: response.insertedId });
+  } else {
+    res.status(500).json({ error: "Failed to create user" });
   }
 };
 
@@ -69,11 +73,9 @@ const updateUser = async (req, res) => {
     .updateOne({ _id: userId }, { $set: updatedUser });
 
   if (response.modifiedCount > 0) {
-    res.status(204).send();
+    res.status(200).json({ message: "User updated successfully" });
   } else {
-    res
-      .status(500)
-      .json(response.error || "Some error occurred while updating the user.");
+    res.status(500).json({ error: "Failed to update user" });
   }
 };
 
@@ -85,11 +87,9 @@ const deleteUser = async (req, res) => {
     .collection("users")
     .deleteOne({ _id: userId }, true);
   if (response.deletedCount > 0) {
-    res.status(204).send();
+    res.status(200).json({ message: "User deleted successfully" });
   } else {
-    res
-      .status(500)
-      .json(response.error || "Some error occurred while deleting the user.");
+    res.status(500).json({ error: "Failed to delete user" });
   }
 };
 
