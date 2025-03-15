69f9e1442bab21c227f250c68afda65433147789
jm-connell <con18013@byui.edu>
2025-03-15 17:13:27 -0600

    make update fields optional

--- controllers/users.js
+++ controllers/users.js
@@ -1,19 +1,23 @@
-const mongodb = require('../db/connect');
-const ObjectId = require('mongodb').ObjectId;
+const mongodb = require("../db/connect");
+const ObjectId = require("mongodb").ObjectId;
 
 const getAllUsers = async (req, res) => {
-  const result = await mongodb.getDb().db().collection('users').find();
+  const result = await mongodb.getDb().db().collection("users").find();
   result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
+    res.setHeader("Content-Type", "application/json");
     res.status(200).json(lists);
   });
 };
 
 const getUserById = async (req, res) => {
   const userId = new ObjectId(req.params.id);
-  const result = await mongodb.getDb().db().collection('users').find({ _id: userId });
+  const result = await mongodb
+    .getDb()
+    .db()
+    .collection("users")
+    .find({ _id: userId });
   result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
+    res.setHeader("Content-Type", "application/json");
     res.status(200).json(lists[0]);
   });
 };
@@ -23,33 +27,53 @@ const createUser = async (req, res) => {
     firstName: req.body.firstName,
     lastName: req.body.lastName,
     email: req.body.email,
-    username: req.body.username
+    username: req.body.username,
   };
-  const response = await mongodb.getDb().db().collection('users').insertOne(user);
+  const response = await mongodb
+    .getDb()
+    .db()
+    .collection("users")
+    .insertOne(user);
   if (response.acknowledged) {
     res.status(201).json(response);
   } else {
-    res.status(500).json(response.error || 'Some error occurred while creating the user.');
+    res
+      .status(500)
+      .json(response.error || "Some error occurred while creating the user.");
   }
 };
 
 const updateUser = async (req, res) => {
   const userId = new ObjectId(req.params.id);
-  const user = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    username: req.body.username
+  const existingUser = await mongodb
+    .getDb()
+    .db()
+    .collection("users")
+    .findOne({ _id: userId });
+
+  if (!existingUser) {
+    return res.status(404).json({ error: "User not found" });
+  }
+
+  const updatedUser = {
+    firstName: req.body.firstName || existingUser.firstName,
+    lastName: req.body.lastName || existingUser.lastName,
+    email: req.body.email || existingUser.email,
+    username: req.body.username || existingUser.username,
   };
+
   const response = await mongodb
     .getDb()
     .db()
-    .collection('users')
-    .replaceOne({ _id: userId }, user);
+    .collection("users")
+    .updateOne({ _id: userId }, { $set: updatedUser });
+
   if (response.modifiedCount > 0) {
     res.status(204).send();
   } else {
-    res.status(500).json(response.error || 'Some error occurred while updating the user.');
+    res
+      .status(500)
+      .json(response.error || "Some error occurred while updating the user.");
   }
 };
 
@@ -58,12 +82,14 @@ const deleteUser = async (req, res) => {
   const response = await mongodb
     .getDb()
     .db()
-    .collection('users')
+    .collection("users")
     .deleteOne({ _id: userId }, true);
   if (response.deletedCount > 0) {
     res.status(204).send();
   } else {
-    res.status(500).json(response.error || 'Some error occurred while deleting the user.');
+    res
+      .status(500)
+      .json(response.error || "Some error occurred while deleting the user.");
   }
 };
 
@@ -72,5 +98,5 @@ module.exports = {
   getUserById,
   createUser,
   updateUser,
-  deleteUser
+  deleteUser,
 };
