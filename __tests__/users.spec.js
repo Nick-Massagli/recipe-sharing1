d36f36f062510148a91916ec816574d8d11e1bee
jwatkinsm <jwatkinsm@gmail.com>
2025-03-19 18:26:33 -0400

    user test code added unable to pass tests

---
+++ __tests__/users.spec.js
@@ -0,0 +1,54 @@
+const app = require('../server')
+const supertest = require('supertest');
+const { expect } = require('@jest/globals');
+const request = supertest(app)
+
+describe('Test Handlers', () => {
+    test('responds to /', async () => {
+        const res = await request.get('/');
+        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+        expect(res.statusCode).toBe(200)
+    })
+
+    test('responds to /users', async () => {
+        const res = await request.get('/users');
+        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+        expect(res.statusCode).toBe(200)
+    })
+
+    test('responds to /users/:id', async () => {
+        const res = await request.get('/users/67d62fe58648e07250d53b77');
+        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+        expect(res.statusCode).toBe(200)
+    })
+
+    test('responds to post /users', async () => {
+        const res = await request.post('/users').send(
+            {
+            firstName: "Emily",
+            lastName: "Button",
+            email: "emilyButton@gmail.com",
+            username: embutton,
+        });
+        expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+        expect(res.statusCode).toBe(201)
+    })
+
+    test('responds to put /users/:id', async () => {
+        const res = await request.put('/users/67d855cb4429c99829059bc4').send(
+            {
+            firstName: "Emily",
+            lastName: "Button",
+            email: "any",
+            username: "embutton",
+});
+expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+        expect(res.statusCode).toBe(201)
+})
+
+test('responds to delete /users/:id', async () => {
+    const res = await request.delete('/users/67d855cb4429c99829059bc4');
+    expect(res.header['content-type']).toBe('application/json; charset=utf-8');
+    expect(res.statusCode).toBe(204)
+})
+})
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
 const mongodb = require('./db/connect');
+var path = require('path');
 
 const port = process.env.PORT || 8080;
 const app = express();
