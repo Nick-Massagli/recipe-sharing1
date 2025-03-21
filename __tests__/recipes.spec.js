d69ef1276b6cff514c5068c9c76b976b3fd8fa4a
jwatkinsm <jwatkinsm@gmail.com>
2025-03-21 12:53:21 -0400

    added in the recipe tests

--- __tests__/recipes.spec.js
+++ __tests__/recipes.spec.js
@@ -4,26 +4,22 @@ const { expect } = require('@jest/globals');
 const request = supertest(server)
 
 describe('Test Handlers', () => {
-    test('responds to /', async () => {
-        const res = await request.get('/');
-        expect(res.header['content-type']).toBe('text/html; charset=utf-8');
-        expect(res.statusCode).toBe(200)
-    })
+ 
 
     test('responds to /users', async () => {
-        const res = await request.get('/users');
+        const res = await request.get('/recipes');
         expect(res.header['content-type']).toBe('application/json; charset=utf-8');
         expect(res.statusCode).toBe(200)
     })
 
-    test('responds to /users/:id', async () => {
-        const res = await request.get('/users/67d62fe58648e07250d53b77');
+    test('responds to /recipe/:id', async () => {
+        const res = await request.get('/recipe/67d62fe58648e07250d53b77');
         expect(res.header['content-type']).toBe('application/json; charset=utf-8');
         expect(res.statusCode).toBe(200)
     })
 
-    test('responds to post /users', async () => {
-        const res = await request.post('/users').send(
+    test('responds to post /recipe', async () => {
+        const res = await request.post('/recipe').send(
             {
             firstName: "Emily",
             lastName: "Button",
@@ -34,8 +30,8 @@ describe('Test Handlers', () => {
         expect(res.statusCode).toBe(201)
     })
 
-    test('responds to put /users/:id', async () => {
-        const res = await request.put('/users/67d855cb4429c99829059bc4').send(
+    test('responds to put /recipe/:id', async () => {
+        const res = await request.put('/recipe/67d855cb4429c99829059bc4').send(
             {
             firstName: "Emily",
             lastName: "Button",
@@ -46,8 +42,8 @@ expect(res.header['content-type']).toBe('application/json; charset=utf-8');
         expect(res.statusCode).toBe(204)
 })
 
-test('responds to delete /users/:id', async () => {
-    const res = await request.delete('/users/67dc2e9e49cf5201e572efad');
+test('responds to delete /recipe/:id', async () => {
+    const res = await request.delete('/recipe/67dc2e9e49cf5201e572efad');
     //expect(res.header['content-type']).toBe('application/json; charset=utf-8');
     expect(res.statusCode).toBe(204)
 })
