de1144d11b7c0efe6ef895836fbc1c358e27e284
jwatkinsm <jwatkinsm@gmail.com>
2025-04-05 19:02:14 -0400

    recipe validation

--- Utilities/valdiation.js
+++ Utilities/valdiation.js
@@ -1,4 +1,5 @@
 const validator = require("./validator");
+const mongoose = require('mongoose');
 
 const saveUser = (req, res, next) => {
     const validationRule = {
@@ -20,6 +21,47 @@ const saveUser = (req, res, next) => {
     });
   };
 
+  const recipe = (req, res, next) => {
+
+      const validationRule = {
+        title: "required|string",
+        ingredients: "required|string",
+        directions: "required|string",
+        prepTime: "required|string",
+        serves: "required|string",
+        //createdTimestamp: "required|date",
+        AuthorID: "required|string",
+      };
+  
+      validator(req.body, validationRule, {}, (err, status) => {
+        if (!status) {
+          return res.status(412).send({
+            success: false,
+            message: "Validation failed",
+            data: err,
+          });
+        }
+    
+        // Validate that AuthorID is a valid ObjectId
+        if (!mongoose.Types.ObjectId.isValid(req.body.AuthorID)) {
+          return res.status(412).send({
+            success: false,
+            message: "Validation failed",
+            data: { AuthorID: "Invalid ObjectId format" },
+          });
+        }
+    
+        next();
+      });
+    };
+    
+
+ 
+ 
+  
+  
   module.exports = {
     saveUser,
+    recipe,
+    
   };
--- routes/recipe.js
+++ routes/recipe.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const recipeController = require('../controllers/recipe');
 const { requiresAuth } = require("express-openid-connect");
+const validator = require("../Utilities/valdiation");
 
 router.get('/', recipeController.getAll);
 router.get('/:id', recipeController.getSingle);
-router.post('/', /*requiresAuth(),*/ recipeController.createRecipe);
-router.put('/:id',/* requiresAuth(),*/ recipeController.updateRecipe);
+router.post('/', validator.recipe, /*requiresAuth(),*/ recipeController.createRecipe);
+router.put('/:id',validator.recipe,/* requiresAuth(),*/ recipeController.updateRecipe);
 router.delete('/:id', recipeController.deleteRecipe);
 module.exports = router;
