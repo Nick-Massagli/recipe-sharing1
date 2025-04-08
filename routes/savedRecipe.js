46c69d70be066257e3e8a5a93c0e47df7252be81
jm-connell <con18013@byui.edu>
2025-04-08 15:55:33 -0600

    use errorHandler middleware in savedRecipes

--- routes/savedRecipe.js
+++ routes/savedRecipe.js
@@ -3,9 +3,45 @@ const router = express.Router();
 const savesController = require("../controllers/savedRecipe");
 const { requiresAuth } = require("express-openid-connect");
 const validator = require("../Utilities/valdiation");
-router.get('/', savesController.getAll);
-router.get('/:id', savesController.getSingle);
-router.post('/',validator.saveRecipe, /*requiresAuth(),*/ savesController.createRecipe);
-//router.put('/:id', requiresAuth(),  savesController.updateRecipe);
-router.delete('/:id',/* requiresAuth(),*/ savesController.deleteRecipe);
+
+router.get("/", async (req, res, next) => {
+  try {
+    await savesController.getAll(req, res);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    await savesController.getSingle(req, res);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/", validator.saveRecipe, async (req, res, next) => {
+  try {
+    await savesController.createRecipe(req, res);
+  } catch (error) {
+    next(error);
+  }
+});
+
+//router.put('/:id', requiresAuth(), async (req, res, next) => {
+//  try {
+//    await savesController.updateRecipe(req, res);
+//  } catch (error) {
+//    next(error);
+//  }
+//});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    await savesController.deleteRecipe(req, res);
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
