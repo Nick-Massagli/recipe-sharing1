3a6a80432f205886d94ea3f65b9ab11a3e8558e0
jm-connell <con18013@byui.edu>
2025-04-08 17:53:57 -0600

    use errorHandler middleware with comments endpoints

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,65 +1,77 @@
 const mongodb = require('../db/connect');
 const { ObjectId } = require('mongodb');
 
-const getComments = async (req, res) => {
-    try {
-        const recipeId = req.params.recipeId;
-        const db = mongodb.getDb();
-        const comments = await mongodb.getDb().db().collection('comments').find({ recipeId: recipeId }).toArray();
-        res.status(200).json(comments);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
-    }
+const getComments = async (req, res, next) => {
+  try {
+    const recipeId = req.params.recipeId;
+    const comments = await mongodb
+      .getDb()
+      .db()
+      .collection('comments')
+      .find({ recipeId: recipeId })
+      .toArray();
+    res.status(200).json(comments);
+  } catch (err) {
+    next(err);
+  }
 };
 
-const addComment = async (req, res) => {
-    try {
-        const recipeId = req.params.recipeId;
-        // Fetch the user from the users collection
-        const user = await mongodb.getDb().db().collection('users').findOne({ _id: new ObjectId(req.body.user) });
-       console.log(user)
-        if (!user) {
-            return res.status(404).json({ message: 'User not found' });
-        }
-        const comment = {
-            recipeId: recipeId,
-            user:{
-                id: user._id,
-                username: user.username,
-                firstName: user.firstName,
-                lastName: user.lastName,
-            },
-            //user: req.body.user,
-            text: req.body.text,
-            createdAt: new Date()
-        };
-        console.log(req.body)
-        //const db = mongodb.getDb();
-        const result = await mongodb.getDb().db().collection('comments').insertOne(comment);
-        res.status(201).json(result);
-    } catch (err) {
-        res.status(500).json({ message: err.message });
+const addComment = async (req, res, next) => {
+  try {
+    const recipeId = req.params.recipeId;
+    const user = await mongodb
+      .getDb()
+      .db()
+      .collection('users')
+      .findOne({ _id: new ObjectId(req.body.user) });
+    if (!user) {
+      const error = new Error('User not found');
+      error.status = 404;
+      throw error;
     }
+    const comment = {
+      recipeId: recipeId,
+      user: {
+        id: user._id,
+        username: user.username,
+        firstName: user.firstName,
+        lastName: user.lastName,
+      },
+      text: req.body.text,
+      createdAt: new Date(),
+    };
+    const result = await mongodb
+      .getDb()
+      .db()
+      .collection('comments')
+      .insertOne(comment);
+    res.status(201).json(result);
+  } catch (err) {
+    next(err);
+  }
 };
 
-const deleteComment = async (req, res) => {
-    try {
-        const commentId = req.params.commentId;
-        //const db = mongodb.getDb();
-        const result = await mongodb.getDb().db().collection('comments').deleteOne({ _id: new ObjectId(commentId) });
-
-        if (result.deletedCount === 0) {
-            return res.status(404).json({ message: 'Comment not found' });
-        }
-
-        res.status(200).json({ message: 'Comment deleted successfully' });
-    } catch (err) {
-        res.status(500).json({ message: err.message });
+const deleteComment = async (req, res, next) => {
+  try {
+    const commentId = req.params.commentId;
+    const result = await mongodb
+      .getDb()
+      .db()
+      .collection('comments')
+      .deleteOne({ _id: new ObjectId(commentId) });
+    if (result.deletedCount === 0) {
+      const error = new Error('Comment not found');
+      error.status = 404;
+      throw error;
     }
+    res.status(200).json({ message: 'Comment deleted successfully' });
+  } catch (err) {
+    next(err);
+  }
 };
 
 module.exports = {
-    getComments,
-    addComment,
-    deleteComment
+  getComments,
+  addComment,
+  deleteComment,
 };
--- routes/comments.js
+++ routes/comments.js
@@ -5,12 +5,26 @@ const validator = require('../Utilities/valdiation');
 const { requiresAuth } = require('express-openid-connect');
 
 // Get all comments for a recipe
-router.get('/:recipeId', validator.getComments, commentsController.getComments);
+router.get('/:recipeId', validator.getComments, (req, res, next) => {
+  commentsController.getComments(req, res, next);
+});
 
 // Add a new comment
-router.post('/:recipeId', validator.saveComment,/* requiresAuth(),*/commentsController.addComment);
+router.post(
+  '/:recipeId',
+  validator.saveComment,
+  /* requiresAuth(), */ (req, res, next) => {
+    commentsController.addComment(req, res, next);
+  }
+);
 
 // Delete a comment
-router.delete('/:recipeId/:commentId',validator.deleteComment,/* requiresAuth(),*/ commentsController.deleteComment);
+router.delete(
+  '/:recipeId/:commentId',
+  validator.deleteComment,
+  /* requiresAuth(), */ (req, res, next) => {
+    commentsController.deleteComment(req, res, next);
+  }
+);
 
 module.exports = router;
