4c368d9147213b792d8c6a398b1b21fba3df0089
jm-connell <con18013@byui.edu>
2025-04-08 12:28:36 -0600

    update users logic to use errorHandler middleware

--- controllers/users.js
+++ controllers/users.js
@@ -1,95 +1,128 @@
 const mongodb = require("../db/connect");
 const ObjectId = require("mongodb").ObjectId;
 
-const getAllUsers = async (req, res) => {
-  const result = await mongodb.getDb().db().collection("users").find();
-  result.toArray().then((lists) => {
-    res.setHeader("Content-Type", "application/json");
-    res.status(200).json(lists);
-  });
-};
-
-const getUserById = async (req, res) => {
-  const userId = new ObjectId(req.params.id);
-  const result = await mongodb
-    .getDb()
-    .db()
-    .collection("users")
-    .find({ _id: userId });
-  result.toArray().then((lists) => {
-    if (lists.length > 0) {
+const getAllUsers = async (req, res, next) => {
+  try {
+    const result = await mongodb.getDb().db().collection("users").find();
+    result.toArray().then((lists) => {
       res.setHeader("Content-Type", "application/json");
-      res.status(200).json(lists[0]);
+      res.status(200).json(lists);
+    });
+  } catch (error) {
+    next(error); // Pass error to the error handler
+  }
+};
+
+const getUserById = async (req, res, next) => {
+  try {
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb
+      .getDb()
+      .db()
+      .collection("users")
+      .find({ _id: userId });
+    result.toArray().then((lists) => {
+      if (lists.length > 0) {
+        res.setHeader("Content-Type", "application/json");
+        res.status(200).json(lists[0]);
+      } else {
+        const error = new Error("User not found");
+        error.status = 404;
+        throw error; // Throw error to be caught by the error handler
+      }
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const createUser = async (req, res, next) => {
+  try {
+    const user = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      username: req.body.username,
+    };
+    const response = await mongodb
+      .getDb()
+      .db()
+      .collection("users")
+      .insertOne(user);
+    if (response.acknowledged) {
+      res
+        .status(201)
+        .json({
+          message: "User created successfully",
+          id: response.insertedId,
+        });
     } else {
-      res.status(404).json({ error: "User not found" });
+      const error = new Error("Failed to create user");
+      error.status = 500;
+      throw error;
     }
-  });
-};
-
-const createUser = async (req, res) => {
-  const user = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    username: req.body.username,
-  };
-  const response = await mongodb
-    .getDb()
-    .db()
-    .collection("users")
-    .insertOne(user);
-  if (response.acknowledged) {
-    res
-      .status(201)
-      .json({ message: "User created successfully", id: response.insertedId });
-  } else {
-    res.status(500).json({ error: "Failed to create user" });
+  } catch (error) {
+    next(error);
   }
 };
 
-const updateUser = async (req, res) => {
-  const userId = new ObjectId(req.params.id);
-  const existingUser = await mongodb
-    .getDb()
-    .db()
-    .collection("users")
-    .findOne({ _id: userId });
+const updateUser = async (req, res, next) => {
+  try {
+    const userId = new ObjectId(req.params.id);
+    const existingUser = await mongodb
+      .getDb()
+      .db()
+      .collection("users")
+      .findOne({ _id: userId });
 
-  if (!existingUser) {
-    return res.status(404).json({ error: "User not found" });
-  }
+    if (!existingUser) {
+      const error = new Error("User not found");
+      error.status = 404;
+      throw error;
+    }
 
-  const updatedUser = {
-    firstName: req.body.firstName || existingUser.firstName,
-    lastName: req.body.lastName || existingUser.lastName,
-    email: req.body.email || existingUser.email,
-    username: req.body.username || existingUser.username,
-  };
+    const updatedUser = {
+      firstName: req.body.firstName || existingUser.firstName,
+      lastName: req.body.lastName || existingUser.lastName,
+      email: req.body.email || existingUser.email,
+      username: req.body.username || existingUser.username,
+    };
 
-  const response = await mongodb
-    .getDb()
-    .db()
-    .collection("users")
-    .updateOne({ _id: userId }, { $set: updatedUser });
+    const response = await mongodb
+      .getDb()
+      .db()
+      .collection("users")
+      .updateOne({ _id: userId }, { $set: updatedUser });
 
-  if (response.modifiedCount > 0) {
-    res.status(200).json({ message: "User updated successfully" });
-  } else {
-    res.status(500).json({ error: "Failed to update user" });
+    if (response.modifiedCount > 0) {
+      res.status(200).json({ message: "User updated successfully" });
+    } else {
+      const error = new Error("Failed to update user");
+      error.status = 500;
+      throw error;
+    }
+  } catch (error) {
+    next(error);
   }
 };
 
-const deleteUser = async (req, res) => {
-  const userId = new ObjectId(req.params.id);
-  const response = await mongodb
-    .getDb()
-    .db()
-    .collection("users")
-    .deleteOne({ _id: userId }, true);
-  if (response.deletedCount > 0) {
-    res.status(204).json({ message: "User deleted successfully" });
-  } else {
-    res.status(500).json({ error: "Failed to delete user" });
+const deleteUser = async (req, res, next) => {
+  try {
+    const userId = new ObjectId(req.params.id);
+    const response = await mongodb
+      .getDb()
+      .db()
+      .collection("users")
+      .deleteOne({ _id: userId }, true);
+    if (response.deletedCount > 0) {
+      res.status(204).json({ message: "User deleted successfully" });
+    } else {
+      const error = new Error("Failed to delete user");
+      error.status = 500;
+      throw error;
+    }
+  } catch (error) {
+    next(error);
   }
 };
 
