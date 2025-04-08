5cad8b96e50f538d34bb52412ddda5982dd65d3a
jm-connell <con18013@byui.edu>
2025-04-08 12:33:15 -0600

    add ValidationError logic in middleware

--- Utilities/errorHandler.js
+++ Utilities/errorHandler.js
@@ -1,6 +1,14 @@
 const errorHandler = (err, req, res, next) => {
   console.error(err.stack);
 
+  if (err.name === "ValidationError") {
+    return res.status(400).json({
+      success: false,
+      message: "Validation Error",
+      errors: err.details || err.errors || null,
+    });
+  }
+
   res.status(err.status || 500).json({
     success: false,
     message: err.message || "Internal Server Error",
