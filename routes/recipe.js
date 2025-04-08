568da3dc58f87f040915d2ecb6b1dc6bef22791d
jm-connell <con18013@byui.edu>
2025-04-08 16:01:55 -0600

    add errorHandler middleware to recipes endpoints

--- routes/recipe.js
+++ routes/recipe.js
@@ -1,12 +1,58 @@
 const express = require('express');
 const router = express.Router();
 const recipeController = require('../controllers/recipe');
-const { requiresAuth } = require("express-openid-connect");
-const validator = require("../Utilities/valdiation");
+const { requiresAuth } = require('express-openid-connect');
+const validator = require('../Utilities/valdiation');
 
-router.get('/', recipeController.getAll);
-router.get('/:id', recipeController.getSingle);
-router.post('/', validator.recipe, /*requiresAuth(),*/ recipeController.createRecipe);
-router.put('/:id',validator.recipe,/* requiresAuth(),*/ recipeController.updateRecipe);
-router.delete('/:id', recipeController.deleteRecipe);
+router.get('/', async (req, res, next) => {
+  try {
+    await recipeController.getAll(req, res);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    await recipeController.getSingle(req, res);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post(
+  '/',
+  validator.recipe,
+  /*requiresAuth(),*/ async (req, res, next) => {
+    try {
+      await recipeController.createRecipe(req, res);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.put(
+  '/:id',
+  validator.recipe,
+  /*requiresAuth(),*/ async (req, res, next) => {
+    try {
+      await recipeController.updateRecipe(req, res);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.delete(
+  '/:id',
+  /*requiresAuth(),*/ async (req, res, next) => {
+    try {
+      await recipeController.deleteRecipe(req, res);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
 module.exports = router;
